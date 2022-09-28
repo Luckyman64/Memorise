21d228d0fbe2e0ce70a2a96cf93e499d875b94af
luckyman64 <clement.lantiat@gmail.com>
2022-09-28 11:03:30 +0200

    CardView

--- Memory/ContentView.swift
+++ Memory/ContentView.swift
@@ -9,20 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            RoundedRectangle(cornerRadius: 25.0)
-                .stroke(lineWidth: 3)
-                .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
-            
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView(isFaceUp: true)
         }
+        .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
         .foregroundColor(.red)
-        
+    }
+}
+
+struct CardView: View {
+    var isFaceUp: Bool = false
+    var body: some View{
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 25.0)
+            if isFaceUp {
+                shape.foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                
+                Text("🚓")
+                    .font(.largeTitle)
+            }
+            else{
+                shape.foregroundColor(.red)
+            }
+                
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
+        ContentView()
+            .preferredColorScheme(.light)
     }
 }
