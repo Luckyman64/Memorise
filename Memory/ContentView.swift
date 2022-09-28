c1135a95426e9f6a8d1f5b7627fa106948d5c082
luckyman64 <clement.lantiat@gmail.com>
2022-09-28 09:32:09 +0200

    Initial Commit

---
+++ Memory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Clement Lantiat on 06/07/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
